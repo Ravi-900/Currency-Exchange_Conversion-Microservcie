fef5e1b530d97c195c0c13f543767bf0038b38df
Ravi Kumawat <ravi900kumawat@gmail.com>
2023-04-29 01:19:12 +0530

    Third Commit

--- currency-conversion-service/src/main/java/com/learn/microservice/currencyconversionservice/CurrencyConersionController.java
+++ currency-conversion-service/src/main/java/com/learn/microservice/currencyconversionservice/CurrencyConersionController.java
@@ -3,21 +3,34 @@ package com.learn.microservice.currencyconversionservice;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@Configuration(proxyBeanMethods = false)
+class RestTemplateConfiguration {
+    
+    @Bean
+    RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+}
+
 @RestController
 public class CurrencyConersionController {
 	
+	@Autowired
 	private CurrencyExchangeProxy proxy;
 	
-	public CurrencyConersionController(CurrencyExchangeProxy proxy) {
-		super();
-		this.proxy = proxy;
-	}
+    @Autowired
+    private RestTemplate restTemplate;
+	
 
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to,
@@ -27,8 +40,10 @@ public class CurrencyConersionController {
 		uriVariables.put("from", from);
 		uriVariables.put("to", to);
 
-		ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate()
+		ResponseEntity<CurrencyConversion> responseEntity = restTemplate
 				.getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
+		
+		
 		CurrencyConversion currencyConversion = responseEntity.getBody();
 
 		return new CurrencyConversion(currencyConversion.getId(), from, to, quantity,
--- currency-exchange-service/src/main/java/com/learn/microservice/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/learn/microservice/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.learn.microservice.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+	
+	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
 	@Autowired
 	private Environment environment;
@@ -21,6 +25,8 @@ public class CurrencyExchangeController {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+		
+		logger.info("retrieveExchangeValue called with with {} to {}",from,to);
 
 		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 		
